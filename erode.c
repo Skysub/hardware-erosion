fc7fe9bedfd082aba870da8807bd8ae259fc3258
s214881 <s214881@student.dtu.dk>
2023-10-25 21:46:21 +0200

    Added an UN-compiled version of erode
    Ved ikke helt i hvilken grad jeg skulle lave assembly udfra C eller C ud fra assembly.
    
    Jeg tænker det er smartest at implementere erode-algoritmen godt ud fra vores ISA i stedet for bare at compile C-kode til assembly.

---
+++ erode.c
@@ -0,0 +1,35 @@
+void erode(unsigned char input_image[20][20], unsigned char output_image[20][20])
+{
+    output_image[0][0] = 0;
+    output_image[0][-1] = 0;
+    output_image[-1][0] = 0;
+    output_image[-1][-1] = 0;
+
+    for (int x = 1; x < 19; x++)
+    {
+        output_image[x][0] = 0;
+        output_image[x][-1] = 0;
+        output_image[0][x] = 0;
+        output_image[-1][x] = 0;
+     
+        for (int y = 1; y < 19; y++)
+        {
+            if (input_image[x][y] == 255)
+            {
+
+                if (input_image[x + 1][y] == 0 ||
+                    input_image[x - 1][y] == 0 ||
+                    input_image[x][y + 1] == 0 ||
+                    input_image[x][y - 1] == 0)
+                {
+                    output_image[x][y] = 0;
+                }
+                else {
+                    output_image[x][y] = 255;
+                }
+            }
+            else
+                output_image[x][y] = 0;
+        }
+    }
+}
